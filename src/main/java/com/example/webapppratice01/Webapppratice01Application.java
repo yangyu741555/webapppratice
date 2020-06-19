57b7f40a16d807823168b7d3db2d5d1e5a981aff
yangyu741555 <204662000@qq.com>
2020-06-19 11:44:14 +0800

    second commit

---
+++ src/main/java/com/example/webapppratice01/Webapppratice01Application.java
@@ -0,0 +1,13 @@
+package com.example.webapppratice01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webapppratice01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Webapppratice01Application.class, args);
+    }
+
+}
